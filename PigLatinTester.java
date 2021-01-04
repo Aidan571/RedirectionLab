f23a66126dbc26a402d4f1ae5c6da89dde0d74f6
Aidan Fingeret <aidan@Aidans-MacBook-Pro.local>
2021-01-04 13:25:51 -0500

    Tested pigLatinSimple.

---
+++ PigLatinTester.java
@@ -0,0 +1,8 @@
+public class PigLatinTester{
+  public static void main(String[]args){
+    System.out.println(PigLatin.pigLatinSimple("mock"));
+    System.out.println(PigLatin.pigLatinSimple("pie"));
+    System.out.println(PigLatin.pigLatinSimple("david"));
+    System.out.println(PigLatin.pigLatinSimple("aaron"));
+  }
+}
